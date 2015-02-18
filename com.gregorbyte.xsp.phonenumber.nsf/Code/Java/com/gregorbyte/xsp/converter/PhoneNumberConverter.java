3613cf7b69b4081e2f99e1fef80382d658db4b41
Cameron Gregor <camerongregor@gmail.com>
2015-02-18 23:20:45 +1100

    Implemented the Converter Version 1

--- com.gregorbyte.xsp.phonenumber.nsf/Code/Java/com/gregorbyte/xsp/converter/PhoneNumberConverter.java
+++ com.gregorbyte.xsp.phonenumber.nsf/Code/Java/com/gregorbyte/xsp/converter/PhoneNumberConverter.java
@@ -1,19 +1,66 @@
 package com.gregorbyte.xsp.converter;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+import com.ibm.commons.util.StringUtil;
 
 public class PhoneNumberConverter implements Converter {
 
-	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+
+		if (StringUtil.isEmpty(value))
+			return value;
+
+		PhoneNumberUtil util = PhoneNumberUtil.getInstance();
+		String defCountryCode = "AU";
+
+		try {
+
+			PhoneNumber number = util.parse(value, defCountryCode);
+
+			if (!util.isValidNumber(number)) {
+
+				FacesMessage fm = new FacesMessage();
+				fm.setSummary("Phone Number is not valid for country code "
+						+ defCountryCode);
+				fm.setDetail("The supplied Phone Number is not valid to the country code "
+								+ defCountryCode);
+				fm.setSeverity(FacesMessage.SEVERITY_ERROR);
+
+				throw new ConverterException(fm);
+
+			} else {
+				return util.format(number, PhoneNumberFormat.INTERNATIONAL);
+			}
+
+		} catch (NumberParseException e) {
+
+			FacesMessage fm = new FacesMessage();
+			fm.setSummary("The Phone Number entered is not in a valid format for "
+							+ defCountryCode);
+			fm.setDetail(e.getMessage());
+			fm.setSeverity(FacesMessage.SEVERITY_ERROR);
+
+			throw new ConverterException(fm);
+		}
 	}
 
-	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+
+		if (value == null)
+			return null;
+		return value.toString();
+
 	}
 
 }
